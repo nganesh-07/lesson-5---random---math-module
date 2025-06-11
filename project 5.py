de615fe8b1359be78e124a3cf33b0f200cfaf2ee
nganesh <nganesh@iu.edu>
2025-06-11 11:17:51 -0700

    project 5

---
+++ project 5.py
@@ -0,0 +1,7 @@
+import math
+
+answer = float(input("Enter an angle in decimal form here:"))
+
+print("The cosin of your input is:", math.cos(answer))
+print("The sin of your input is:", math.sin(answer))
+print("The tangent of your input is:", math.tan(answer))
